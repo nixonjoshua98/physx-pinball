661d8f595bd9cb9ec1cfa2fcd362ead336a524e4
Joshua Nixon <nixonjoshua98@gmail.com>
2020-01-29 14:57:58 +0000

    Removed some redundant code

--- Tutorial 4/PhysicsEngine.h
+++ Tutorial 4/PhysicsEngine.h
@@ -92,65 +92,6 @@ namespace PhysicsEngine
 		void CreateShape(const PxGeometry& geometry, PxReal density=0.f);
 	};
 
-	///Generic scene class
-	class Scene
-	{
-	protected:
-		//a PhysX scene object
-		PxScene* px_scene;
-		//pause simulation
-		bool pause;
-		//selected dynamic actor on the scene
-		PxRigidDynamic* selected_actor;
-		//original and modified colour of the selected actor
-		std::vector<PxVec3> sactor_color_orig;
-		//custom filter shader
-		PxSimulationFilterShader filter_shader;
-
-		void HighlightOn(PxRigidDynamic* actor);
-
-		void HighlightOff(PxRigidDynamic* actor);
-
-	public:
-		Scene(PxSimulationFilterShader custom_filter_shader=PxDefaultSimulationFilterShader) : filter_shader(custom_filter_shader) {}
-
-		///Init the scene
-		void Init();
-
-		///User defined initialisation
-		virtual void CustomInit() {}
-
-		///Perform a single simulation step
-		void Update(PxReal dt);
-
-		///User defined update step
-		virtual void CustomUpdate() {}
-
-		///Add actors
-		void Add(Actor* actor);
-
-		///Get the PxScene object
-		PxScene* Get();
-
-		///Reset the scene
-		void Reset();
-
-		///Set pause
-		void Pause(bool value);
-
-		///Get pause
-		bool Pause();
-
-		///Get the selected dynamic actor on the scene
-		PxRigidDynamic* GetSelectedActor();
-
-		///Switch to the next dynamic actor
-		void SelectNextActor();
-
-		///a list with all actors
-		std::vector<PxActor*> GetAllActors();
-	};
-
 	///Generic Joint class
 	class Joint
 	{
